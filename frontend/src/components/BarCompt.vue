b4241203320d1ef3e2f7ea98342a035c535a212f
Gabriel Godoi <gabrielgodoi987@gmail.com>
2023-08-20 02:21:39 -0300

    ok

--- frontend/src/components/BarCompt.vue
+++ frontend/src/components/BarCompt.vue
@@ -1,40 +1,35 @@
 <template>
-    <q-layout view="lHh lpR lFf">
+    <q-header elevated class="bg-primary text-white" height-hint="98">
+        <q-toolbar squared class="shadow-10">
+            <q-btn class="lt-md" dense flat round icon="menu" @click="toggleLeftDrawer" />
 
-        <q-header elevated class="bg-primary text-white" height-hint="98">
-            <q-toolbar squared class="shadow-10">
-                <q-btn class="lt-md" dense flat round icon="menu" @click="toggleLeftDrawer" />
+            <div class="column" style="margin: 0 auto">
+                <q-toolbar-title class="text-center">
+                    <q-avatar>
+                        <q-icon name="person" />
+                    </q-avatar>
+                </q-toolbar-title>
+                <q-tabs class="gt-sm text-secondary" align="center">
+                    <q-route-tab>Segmento</q-route-tab>
+                    <q-route-tab>Sobre</q-route-tab>
+                    <q-route-tab>Contato</q-route-tab>
+                    <q-route-tab>Portfólios</q-route-tab>
+                    <q-btn outline color="secondary" label="Central" />
+                </q-tabs>
+            </div>
+        </q-toolbar>
+    </q-header>
 
-                <div class="column" style="margin: 0 auto">
-                    <q-toolbar-title class="text-center">
-                        <q-avatar>
-                            <q-icon name="person" />
-                        </q-avatar>
-                    </q-toolbar-title>
-                    <q-tabs class="gt-sm text-secondary" align="center">
-                        <q-route-tab>Segmento</q-route-tab>
-                        <q-route-tab>Sobre</q-route-tab>
-                        <q-route-tab>Contato</q-route-tab>
-                        <q-route-tab>Portfólios</q-route-tab>
-                        <q-btn outline color="secondary" label="Central" />
-                    </q-tabs>
-                </div>
-            </q-toolbar>
-        </q-header>
-
-        <q-drawer class="lt-md" v-model="leftDrawerOpen" side="left" bordered>
-            <!-- drawer content -->
-            <q-tabs vertical class="bg-accent text-center text-secondary">
-                <q-route-tab>Segmento</q-route-tab>
-                <q-route-tab>Sobre</q-route-tab>
-                <q-route-tab>Contato</q-route-tab>
-                <q-route-tab>Portfólios</q-route-tab>
-                <q-btn outline color="secondary" label="Central" />
-            </q-tabs>
-        </q-drawer>
-
-
-    </q-layout>
+    <q-drawer class="lt-md" v-model="leftDrawerOpen" side="left" bordered>
+        <!-- drawer content -->
+        <q-tabs vertical class="bg-accent text-center text-secondary">
+            <q-route-tab>Segmento</q-route-tab>
+            <q-route-tab>Sobre</q-route-tab>
+            <q-route-tab>Contato</q-route-tab>
+            <q-route-tab>Portfólios</q-route-tab>
+            <q-btn outline color="secondary" label="Central" />
+        </q-tabs>
+    </q-drawer>
 </template>
   
 <script>
