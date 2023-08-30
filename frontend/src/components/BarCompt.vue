7df997ba2157fc64c736d2005fa02ad84243cd5e
GabrielGodoi987 <gabrielgodoi987@gmail.com>
2023-08-30 18:15:47 -0300

    começando front end do site

--- frontend/src/components/BarCompt.vue
+++ frontend/src/components/BarCompt.vue
@@ -1,16 +1,33 @@
 <template>
-    <div>
+    <q-header :class="bgColor">
+        <q-toolbar class="justify-around text-weight-bold">
+            <q-avatar>
+                <div class="text-caption">LOGO</div>
+            </q-avatar>
+            <q-tabs>
+                <q-tab>Segmento</q-tab>
+                <q-tab>Sobre</q-tab>
+                <q-tab>Contato</q-tab>
+                <q-tab>Portfólio</q-tab>
+            </q-tabs>
 
-    </div>
+            <q-btn rounded color="primary" label="Comece Agora" />
+        </q-toolbar>
+    </q-header>
 </template>
   
 <script>
 
 export default {
+    props: {
+        //bgColor: String
+    },
     setup() {
 
-        return {
+        const bgColor = "bg-transparent text-black";
 
+        return {
+            bgColor
         }
     }
 }
