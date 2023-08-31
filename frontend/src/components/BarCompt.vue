c20b6527db067dca31cf495883a136bee0ebe0fa
GabrielGodoi987 <gabrielgodoi987@gmail.com>
2023-08-31 01:01:11 -0300

    inicializando design do site

--- frontend/src/components/BarCompt.vue
+++ frontend/src/components/BarCompt.vue
@@ -1,33 +1,52 @@
 <template>
     <q-header :class="bgColor">
         <q-toolbar class="justify-around text-weight-bold">
+            <div class="lt-sm">
+                <q-btn dense flat round icon="menu" @click="MobileMenu = !MobileMenu" />
+            </div>
             <q-avatar>
                 <div class="text-caption">LOGO</div>
             </q-avatar>
-            <q-tabs>
-                <q-tab>Segmento</q-tab>
-                <q-tab>Sobre</q-tab>
-                <q-tab>Contato</q-tab>
-                <q-tab>Portfólio</q-tab>
-            </q-tabs>
-
+            <div class="gt-xs">
+                <q-tabs>
+                    <q-tab>Segmento</q-tab>
+                    <q-tab>Sobre</q-tab>
+                    <q-tab>Contato</q-tab>
+                    <q-tab>Portfólio</q-tab>
+                </q-tabs>
+            </div>
             <q-btn rounded color="primary" label="Comece Agora" />
         </q-toolbar>
+
     </q-header>
+
+    <q-drawer v-model="MobileMenu">
+        <q-tabs vertical swipeable class="q-mt-xl q-gutter-xl">
+            <q-tab>Segmento</q-tab>
+            <br>
+            <q-tab>Sobre</q-tab>
+            <br>
+            <q-tab>Contato</q-tab>
+            <br>
+            <q-tab>Portfólio</q-tab>
+        </q-tabs>
+    </q-drawer>
 </template>
   
 <script>
+import { ref } from 'vue';
 
 export default {
     props: {
-        //bgColor: String
+        bgColor: String
     },
-    setup() {
+    setup(props) {
 
-        const bgColor = "bg-transparent text-black";
+        const MobileMenu = ref(false);
 
         return {
-            bgColor
+            props,
+            MobileMenu
         }
     }
 }
--- frontend/src/views/HomePage.vue
+++ frontend/src/views/HomePage.vue
@@ -1,6 +1,59 @@
 <template>
     <q-layout view="lHh lpR lFf">
-        <BarCompt />
+        <BarCompt bgColor="bg-transparent text-black" />
+        <!-- parte do banner, da página principal -->
+        <div class="row bg-secondary justify-center">
+            <!-- lado de texto e interação de usuário -->
+            <div class="col-md-6 q-mt-xl q-pa-xl">
+                <div class="text-h2 text-weight-bolder">DsTech:</div>
+                <div class="text-h4 text-weight-bolder">além de códigos, transformando sonhos</div>
+                <div class="text-h6 text-weight-regular q-mt-xl">
+                    Transformamos visões empresariais em realidade com interfaces tecnológicas sob medida. Descubra como
+                    nossa expertise impulsiona setores diversos, da saúde ao varejo. Junte-se à revolução tecnológica hoje
+                    mesmo!
+                </div>
+            </div>
+            <!-- segundo lado do banner, com imagem em tamanho completo a div -->
+            <div class="col-md-6">
+                <q-img src="../assets/pexels-lukas-574073.jpg" style="width: 100%; height: 100%;" />
+            </div>
+        </div>
+           
+
+        
+
+
+        <q-page-container class="row justify-around q-mt-xl bg-secondary">
+            <div class="col-md-6">
+                <q-img src="../assets/pexels-elvis-2528118.jpg" ratio="1" />
+            </div>
+            <div class="col-md-5 q-gutter-y-xl q-mt-xl">
+                <div class="text-h6 text-weight-bolder q-mt-lg">Conheça a Nossa <strong class="text-yellow">EPR</strong>
+                    Revolucionária - Transformando Empresas,
+                    Potencializando Sucesso!</div>
+                <div class="text-h6 text-weight-regular text-center">Na DsTech, apresentamos a você uma visão completamente
+                    nova de gestão
+                    empresarial com nossa solução de EPR de última geração. Imagine um sistema que sincroniza cada aspecto
+                    da
+                    sua operação, eliminando o trabalho manual redundante e impulsionando a eficiência a novos patamares.
+                </div>
+
+                <div class="text-h6 text-weight-regular text-center">Prepare-se para otimizar seus processos financeiros,
+                    aprimorar a cadeia
+                    de suprimentos e fortalecer sua estratégia de recursos humanos, tudo isso com um único sistema
+                    abrangente.
+                    Nossa EPR é a chave para desbloquear o potencial oculto da sua empresa.</div>
+
+                <div class="text-h6 text-weight-regular text-center">Curioso para explorar mais? Clique no botão abaixo e
+                    embarque na
+                    jornada para uma gestão empresarial simplificada, inteligente e mais lucrativa. Seu futuro empresarial
+                    aguarda, e a DsTech está pronta para acompanhá-lo nessa transformação extraordinária.</div>
+                <div class="text-center q-gutter-x-xl">
+                    <q-btn rounded label="Saiba Mais" color="primary"/>
+                    <q-btn rounded label="Fale Conosco" color="accent"/>
+                </div>
+            </div>
+        </q-page-container>
     </q-layout>
 </template>
 
